3b254c6c22266d0e381bda4c2660dc6c07d36c25
Stephen Richer <sr467@bath.ac.uk>
2019-08-02 10:40:24 +0100

    Retrieve python3 script directory.

---
+++ common_tools/script_path.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Retrieve absolute directory of python3 script. Useful for calling bash scripts
+from python3 that are stored in the same directory as the python3 script.
+"""
+
+import os, logging
+
+def script_path():
+    
+    """ Return absolute directory of python3 script. """
+    
+    fun_name = sys._getframe().f_code.co_name
+    log = logging.getLogger(f'{__name__}.{fun_name}')
+    
+    try:
+        path = '/'.join(os.path.realpath(__file__).split('/')[0:-1]) + '/'
+    except NameError:
+        log.error('Function was not called from python3 script.')
+        raise
+    
+    return path
