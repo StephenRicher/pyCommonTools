2f7acda925dd1ce22248bcdb3cbaa7aff77b89fb
Stephen Richer <sr467@bath.ac.uk>
2019-09-26 10:24:01 +0100

    Updated.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'common_tools'
+NAME = 'pyCommonTools'
 DESCRIPTION = ('Set of tools to perform common tasks across bioinformatics '
                'tools. These include reading/writing SAM/BAM/GZIP files '
                'and exception logging.')
@@ -20,40 +20,30 @@ URL = 'https://github.com/StephenRicher/common_tools'
 EMAIL = 'sr467@bath.ac.uk'
 AUTHOR = 'Stephen Richer'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
 REQUIRED = []
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+def get_version():
+    try:
+        with io.open(os.path.join(here, f'{NAME}/_version.py'), 
+                encoding='utf-8') as f:
+            for line in f:
+                mo = re.match("__version__ = '([^']+)'", line)
+                if mo:
+                    ver = mo.group(1)
+                    return ver
+        return None
+    except FileNotFoundError:
+        return None
+
 # Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    with io.open(os.path.join(here, 'README.md'), 
+            encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
 
 class UploadCommand(Command):
     """Support setup.py upload."""
@@ -86,46 +76,29 @@ class UploadCommand(Command):
         os.system('twine upload dist/*')
 
         self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
+        os.system(f'git tag v{get_version()}')
         os.system('git push --tags')
 
         sys.exit()
 
-
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
+setup(name = NAME,
+    version = get_version(),
+    description = DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = AUTHOR,
+    author_email = EMAIL,
+    python_requires = REQUIRES_PYTHON,
+    url = URL,
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = REQUIRED,
+    scripts = SCRIPTS,
+    setup_requires = ['pytest-runner'],
+    tests_require = ['pytest'],
+    zip_safe = False,
+    # $ python setup.py upload
     cmdclass={
         'upload': UploadCommand,
-    },
+    }
 )
