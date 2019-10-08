d26e7fe1a9ff1fc563b0ca353c1701eba045f2a9
Stephen Richer <sr467@bath.ac.uk>
2019-10-08 16:11:03 +0100

    Added functions to initialise and create a logger.

--- pyCommonTools/exception_logger.py
+++ pyCommonTools/exception_logger.py
@@ -2,15 +2,30 @@
 
 import sys, logging
 
-def handle_exception(exc_type, exc_value, exc_traceback):
+def create_logger():
+    # Get name of function that called create_logger()
+    function_name = sys._getframe(1).f_code.co_name
+    return logging.getLogger(f'{__name__}.{function_name}')
     
+def initiliase_logger(
+        log_output = None,
+        log_level = logging.DEBUG,
+        log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
+
+    (logging.basicConfig(
+        filename = log_output,
+        format = log_format,
+        level = log_level))
+    sys.excepthook = log_uncaught_exception
+
+def log_uncaught_exception(exc_type, exc_value, exc_traceback):
+
     ''' Redirect uncaught exceptions (excluding KeyboardInterrupt) 
         through the logging module. 
     '''
-    
-    fun_name = sys._getframe().f_code.co_name
-    log = logging.getLogger(f'{__name__}.{fun_name}')
-    
+
+    log = create_logger()
+
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
