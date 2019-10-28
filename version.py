91a22a3a0ad022386b21ec21a0f0695dfaa5cbac
Stephen Richer <sr467@bath.ac.uk>
2019-10-28 10:11:52 +0000

    Updated version to 1.0.0.

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-_MAJOR_VERSION = '0'
-_MINOR_VERSION = '2'
-_PATCH_VERSION = '2'
+_MAJOR_VERSION = '1'
+_MINOR_VERSION = '0'
+_PATCH_VERSION = '0'
 
 _VERSION_SUFFIX = None
 
