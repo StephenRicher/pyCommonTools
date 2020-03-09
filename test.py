da9080585e5213337b55c36ab1a054e8e3524227
Stephen Richer <sr467@bath.ac.uk>
2020-03-09 12:07:31 +0000

    Add epilog to within pyCommonTools and reuse epilog by default across subparsers.

--- test.py
+++ test.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python
 
 """
-Template script for demonstarting pyCommonTools argeparse commands. 
+Template script for demonstarting pyCommonTools argeparse commands.
 """
 
 import sys
 import pyCommonTools as pct
 import argparse
+from version import __version__
 
 def main():
 
     epilog = 'Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
-    
-    parser = pct.make_parser(epilog = epilog)
-    
+
+    parser = pct.make_parser(epilog = epilog, version=__version__)
+
     base_args = pct.get_base_args()
-    inout_args = pct.get_inout_args()
+    in_arg = pct.get_in_arg()
     subparser = pct.make_subparser(parser)
 
     run_parser = subparser.add_parser(
         'run', help='Reverse file contents',
         description=run.__doc__,
-        parents=[base_args, inout_args],
-        epilog=epilog)
+        parents=[base_args, in_arg],
+        epilog=parser.epilog)
     run_parser.add_argument(
         '-n', '--number', default=[0],
         help='Enter a number. (default: %(default)s)')
-       
+
     run_parser.set_defaults(function=run)
-    
+
     return (pct.execute(parser))
-    
 
-def run(infile, out, number):
 
+def run(infile, number):
+
+    log = pct.create_logger()
+    log.debug('hello')
     with pct.open(infile) as f:
-        with pct.open(out, 'w') as fo:
-            for line in f:
-                fo.write(line[::-1])
+        for line in f:
+            print(line[::-1])
 
 
 main()
